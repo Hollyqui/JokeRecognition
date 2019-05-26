c7c36ba6b78431f1e1107aa64365b893f9b1a14e
Felix Quinque <40171911+Hollyqui@users.noreply.github.com>
2019-05-26 17:07:54 +0200

    Histogram Plotter Added

---
+++ Histogram_Plot.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+
+def plot_histogram(labels, predictions):
+    class_var = np.array( [predictions[i][0] for i in range(len(predictions)) ] ) # list of prob_pion
+    df_test = pd.DataFrame()
+    df_test["Net"] = class_var
+    df_test["absid"] = labels.T[0]
+
+    crit_class1 = df_test['absid'] == 1
+    crit_class2 = df_test['absid'] == 0
+    df_test_class1 = df_test[crit_class1]
+    df_test_class2 = df_test[crit_class2]
+
+    df_test_class1["Net"].plot.hist(bins=50, range=(0,1), alpha=0.5, density=True, label="Headline") # log="y")
+    df_test_class2["Net"].plot.hist(bins=50, range=(0,1), alpha=0.5, density=True, label="Joke") #log="y")
+    #df_test_protons["myMLP"].plot.hist(bins=50, range=(0,1), alpha=0.5, density=True, label="protons")
+    plt.legend(loc='upper right')
+    plt.xlabel("ConvNet classifier")
+    plt.show()
+
+    f = plt.figure()
+    f.savefig("performance.pdf", bbox_inches='tight')
+
+labels =  np.load("labels_headlines.npy")
+predictions = np.load("predictions_headlines.npy")
+plot_histogram(labels, predictions)
