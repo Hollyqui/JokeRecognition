152f43987297ea25e23de065c42db05cbb5edca4
Felix Quinque <40171911+Hollyqui@users.noreply.github.com>
2019-05-26 17:10:15 +0200

    comments added

--- Histogram_Plot.py
+++ Histogram_Plot.py
@@ -5,6 +5,7 @@ import tensorflow as tf
 import numpy as np
 import pandas as pd
 
+# function plots a histogram for any binary classifier; needs input of labels and network predictions
 def plot_histogram(labels, predictions):
     class_var = np.array( [predictions[i][0] for i in range(len(predictions)) ] ) # list of prob_pion
     df_test = pd.DataFrame()
@@ -15,10 +16,9 @@ def plot_histogram(labels, predictions):
     crit_class2 = df_test['absid'] == 0
     df_test_class1 = df_test[crit_class1]
     df_test_class2 = df_test[crit_class2]
-
+    #log="y" transforms the scale to be logarithmic (in the following two lines)
     df_test_class1["Net"].plot.hist(bins=50, range=(0,1), alpha=0.5, density=True, label="Headline") # log="y")
-    df_test_class2["Net"].plot.hist(bins=50, range=(0,1), alpha=0.5, density=True, label="Joke") #log="y")
-    #df_test_protons["myMLP"].plot.hist(bins=50, range=(0,1), alpha=0.5, density=True, label="protons")
+    df_test_class2["Net"].plot.hist(bins=50, range=(0,1), alpha=0.5, density=True, label="Joke") #log="y") 
     plt.legend(loc='upper right')
     plt.xlabel("ConvNet classifier")
     plt.show()
